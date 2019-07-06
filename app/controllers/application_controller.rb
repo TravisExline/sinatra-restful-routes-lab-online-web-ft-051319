34b549cab3ec09f79f931553f018aabf75108e10
Travis Exline <t.exline22@gmail.com>
2019-07-06 11:53:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,31 +4,31 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/recipes' do 
+  get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/new' do 
-    erb :new 
+  get '/recipes/new' do
+    erb :new
   end
 
-  post '/recipes' do 
+  post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect to "/recipes/#{@recipe.id}"
   end
 
- get '/recipes/:id' do 
+ get '/recipes/:id' do
    @recipe = Recipe.find_by_id(params[:id])
-   erb :show 
- end 
+   erb :show
+ end
 
- get '/recipes/:id/edit' do 
+ get '/recipes/:id/edit' do
    @recipe = Recipe.find_by_id(params[:id])
-   erb :edit 
- end 
+   erb :edit
+ end
 
-  patch '/recipes/:id' do 
+  patch '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name= params[:name]
     @recipe.ingredients = params[:ingredients]
